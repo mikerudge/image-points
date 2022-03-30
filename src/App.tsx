96702a90a42c56662005d144a6d99c2eb0a5ebdd
Mike Rudge <mike@biglemon.co.uk>
2022-03-30 16:32:14 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,294 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useRef, useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css"
+import { Col, Row, Select } from "antd"
+import { DownCircleTwoTone } from "@ant-design/icons"
+import { bodyParts } from "./bodyParts"
+import { data as bodyData } from "./data"
+const generatedData = (): Data[] => {
+	// generate an array of 32 objects
+	const data: Data[] = []
+	let x = 10
+	let y = 10
+
+	for (let i = 0; i < 33; i++) {
+		data.push({
+			x: x,
+			y: y,
+			bodyPart: i,
+		})
+
+		x += 90
+
+		if (x > 800) {
+			x = 10
+			y += 140
+		}
+	}
+	return data
 }
 
-export default App;
+const demoData: Data[] = [
+	// Nose
+	{ x: 200, y: 30, bodyPart: 0 },
+	// Left eye
+	{ x: 240, y: 10, bodyPart: 1 },
+	{ x: 280, y: 10, bodyPart: 2 },
+	{ x: 320, y: 10, bodyPart: 3 },
+	// Right Eye
+	{ x: 160, y: 10, bodyPart: 4 },
+	{ x: 130, y: 10, bodyPart: 5 },
+	{ x: 90, y: 10, bodyPart: 6 },
+	// Left Ear
+	{ x: 360, y: 30, bodyPart: 7 },
+	// Right Ear
+	{ x: 50, y: 30, bodyPart: 8 },
+	// Mouth
+	{ x: 170, y: 50, bodyPart: 9 },
+	{ x: 230, y: 50, bodyPart: 10 },
+	// Body
+	{ x: 300, y: 150, bodyPart: 11 },
+	{ x: 100, y: 150, bodyPart: 12 },
+	{ x: 350, y: 250, bodyPart: 13 },
+	{ x: 50, y: 250, bodyPart: 14 },
+	{ x: 400, y: 200, bodyPart: 15 },
+	{ x: 30, y: 200, bodyPart: 16 },
+	{ x: 480, y: 200, bodyPart: 17 },
+	{ x: 2, y: 200, bodyPart: 18 },
+
+	{ x: 450, y: 100, bodyPart: 19 },
+	{ x: 10, y: 150, bodyPart: 20 },
+	{ x: 400, y: 100, bodyPart: 21 },
+	{ x: 50, y: 150, bodyPart: 22 },
+	{ x: 300, y: 400, bodyPart: 23 },
+	{ x: 100, y: 400, bodyPart: 24 },
+	{ x: 350, y: 550, bodyPart: 25 },
+	{ x: 50, y: 550, bodyPart: 26 },
+	{ x: 350, y: 750, bodyPart: 27 },
+	{ x: 50, y: 750, bodyPart: 28 },
+	{ x: 330, y: 790, bodyPart: 29 },
+	{ x: 70, y: 790, bodyPart: 30 },
+	{ x: 378, y: 790, bodyPart: 31 },
+	{ x: 2, y: 790, bodyPart: 32 },
+]
+
+type Data = {
+	x: number
+	y: number
+	bodyPart: number
+}
+const image = {
+	width: 3268 / 4,
+	height: 3989 / 4,
+}
+const formattedData = bodyData.map((d, i) => {
+	return {
+		x: d.x * image.width,
+		y: d.y * image.height,
+		bodyPart: i,
+	}
+})
+
+function App() {
+	const ref = useRef<HTMLCanvasElement>(null)
+	const [data, setData] = useState<Data[]>(formattedData)
+	const mouseDown = useRef<boolean>(false)
+
+	useEffect(() => {
+		if (ref.current) {
+			const cxt = ref.current.getContext("2d")
+
+			// clear the canvas
+
+			const drawCircleAndLine = (dataList: Data[]) => {
+				if (cxt) {
+					dataList.forEach((item) => {
+						const bodyPartId = item.bodyPart
+
+						// draw a circle
+						cxt.beginPath()
+						cxt.arc(item.x, item.y, 5, 0, 2 * Math.PI)
+						cxt.fillStyle = "palevioletred"
+						cxt.fill()
+						cxt.lineWidth = 1
+						cxt.strokeStyle = "palevioletred"
+
+						cxt.closePath()
+
+						// Add body part number to the circle
+						// cxt.font = "20px system-ui"
+						// cxt.fillStyle = "black"
+						// cxt.fillText(bodyPartId.toString(), data.x - 5, data.y + 5)
+
+						// find the connections for this body part
+						const connections = bodyParts.find(
+							(part) => part.id === bodyPartId
+						)?.connects
+						console.log("connections", connections)
+
+						// draw a line to each connection
+						if (connections) {
+							connections.forEach((connection) => {
+								const connectionData = dataList.find(
+									(d) => d.bodyPart === connection
+								)
+
+								if (connectionData) {
+									console.log("connectionData", connectionData)
+
+									cxt.beginPath()
+									cxt.moveTo(item.x, item.y)
+									cxt.lineTo(connectionData.x, connectionData.y)
+									cxt.strokeStyle = "lightblue"
+									cxt.stroke()
+									cxt.closePath()
+								}
+							})
+						}
+					})
+				}
+			}
+
+			if (cxt) {
+				// Clear all canvas
+				cxt.clearRect(0, 0, ref.current.width, ref.current.height)
+			}
+			drawCircleAndLine(data)
+		}
+
+		return () => {}
+	}, [ref, data])
+
+	const onFilter = (id: number | string) => {
+		if (typeof id === "string") {
+			id = parseInt(id)
+		}
+
+		const connections = bodyParts.find((part) => part.id === id)?.connects
+
+		if (connections && (connections?.length ?? 0) > 0) {
+			const newData = formattedData.filter((d) =>
+				connections.includes(d.bodyPart)
+			)
+			console.log("newData", newData)
+			setData(newData)
+		} else {
+			const newData = formattedData.filter((d) => d.bodyPart === id)
+			setData(newData)
+		}
+	}
+
+	const movePoint = (id: number | string, x: number, y: number) => {
+		if (typeof id === "string") {
+			id = parseInt(id)
+		}
+
+		const newData = data.map((d) => {
+			if (d.bodyPart === id) {
+				return {
+					...d,
+					x: x,
+					y: y,
+				}
+			}
+			return d
+		})
+		setData(newData)
+	}
+
+	const handleMouseDown = (
+		e: React.MouseEvent<HTMLCanvasElement, MouseEvent>
+	) => {
+		// Do nothing
+
+		e.preventDefault()
+		e.stopPropagation()
+
+		console.log("evt", e)
+
+		if (ref.current) {
+			const ctx = ref.current.getContext("2d")
+			if (ctx) {
+				const x = e.clientX - ref.current.offsetLeft
+				const y = e.clientY - ref.current.offsetTop
+
+				const data = formattedData.find((d) => {
+					const dx = Math.abs(d.x - x)
+					const dy = Math.abs(d.y - y)
+					return dx < 10 && dy < 10
+				})
+
+				if (data) {
+					console.log("data", data)
+					movePoint(data.bodyPart, x, y)
+				}
+			}
+		}
+
+		const x = e.clientX
+		const y = e.clientY
+	}
+
+	return (
+		<Row
+			style={{
+				minHeight: "100vh",
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				textAlign: "center",
+			}}
+		>
+			<div>
+				<Select onChange={(v) => onFilter(v)}>
+					{/* Generate an array from 0 - 32 */}
+					{Array.from(Array(32).keys()).map((i) => (
+						<Select.Option key={i} value={i}>
+							{i}
+						</Select.Option>
+					))}
+				</Select>
+			</div>
+			<div style={{ width: "100vw" }}>
+				<h2 style={{ marginBottom: "2rem" }}>Image positing test</h2>
+
+				<div>
+					<div
+						style={{
+							position: "relative",
+							display: "inline-block",
+							width: image.width,
+							height: image.height,
+						}}
+					>
+						<img
+							style={{
+								position: "absolute",
+								top: 0,
+								left: 0,
+								width: "100%",
+								height: "100%",
+							}}
+							src={"input_image.jpeg"}
+							className=''
+							alt='test points'
+						/>
+						<canvas
+							style={{
+								position: "absolute",
+								zIndex: 10,
+								top: 0,
+								left: 0,
+							}}
+							ref={ref}
+							width={image.width}
+							height={image.height}
+						></canvas>
+					</div>
+				</div>
+			</div>
+		</Row>
+	)
+}
+
+export default App
---
+++ src/bodyParts.ts
@@ -0,0 +1,167 @@
+export const bodyParts = [
+	{
+		id: 0,
+		name: "nose",
+		connects: [1, 4],
+	},
+	{
+		id: 1,
+		name: "left_eye_inner",
+		connects: [2],
+	},
+	{
+		id: 2,
+		name: "left_eye",
+		connects: [3],
+	},
+	{
+		id: 3,
+		name: "left_eye_outer",
+		connects: [7],
+	},
+	{
+		id: 4,
+		name: "right_eye_inner",
+		connects: [5],
+	},
+	{
+		id: 5,
+		name: "right_eye",
+		connects: [6],
+	},
+	{
+		id: 6,
+		name: "right_eye_outer",
+		connects: [8],
+	},
+	{
+		id: 7,
+		name: "left_ear",
+		connects: [3],
+	},
+	{
+		id: 8,
+		name: "right_ear",
+		connects: [6],
+	},
+	{
+		id: 9,
+		name: "mouth_left",
+		connects: [10],
+	},
+	{
+		id: 10,
+		name: "mouth_right",
+		connects: [],
+	},
+	{
+		id: 11,
+		name: "left_shoulder",
+		connects: [12, 13, 23],
+	},
+	{
+		id: 12,
+		name: "right_shoulder",
+		connects: [14, 24],
+	},
+	{
+		id: 13,
+		name: "left_elbow",
+		connects: [15],
+	},
+	{
+		id: 14,
+		name: "right_elbow",
+		connects: [16],
+	},
+	{
+		id: 15,
+		name: "left_wrist",
+		connects: [17, 21, 19],
+	},
+	{
+		id: 16,
+		name: "right_wrist",
+		connects: [18, 22],
+	},
+	{
+		id: 17,
+		name: "left_pinky",
+		connects: [19],
+	},
+	{
+		id: 18,
+		name: "right_pinky",
+		connects: [20],
+	},
+	{
+		id: 19,
+		name: "left_index",
+		connects: [],
+	},
+	{
+		id: 20,
+		name: "right_index",
+		connects: [16, 18],
+	},
+	{
+		id: 21,
+		name: "left_thumb",
+		connects: [],
+	},
+	{
+		id: 22,
+		name: "right_thumb",
+		connects: [],
+	},
+	{
+		id: 23,
+		name: "left_hip",
+		connects: [24, 25],
+	},
+	{
+		id: 24,
+		name: "right_hip",
+		connects: [26],
+	},
+	{
+		id: 25,
+		name: "left_knee",
+		connects: [27],
+	},
+	{
+		id: 26,
+		name: "right_knee",
+		connects: [28],
+	},
+	{
+		id: 27,
+		name: "left_ankle",
+		connects: [29, 31],
+	},
+	{
+		id: 28,
+		name: "right_ankle",
+		connects: [30, 32],
+	},
+	{
+		id: 29,
+		name: "left_heel",
+		connects: [31],
+	},
+	{
+		id: 30,
+		name: "right_heel",
+		connects: [32],
+	},
+	{
+		id: 31,
+		name: "left_foot_index",
+		connects: [],
+	},
+	{
+		id: 32,
+		name: "right_foot_index",
+		connects: [],
+	},
+]
